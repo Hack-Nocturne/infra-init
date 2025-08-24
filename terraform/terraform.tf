879426c07aed50253c7dd624278c43e539cee107
Rishabh Kumar <rishabh.kumar.pro@gmail.com>
2025-08-24 13:03:39 +0530

    Add Cloudflare R2 based S3 backend to synchronize tfstate

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,18 @@
 terraform {
   required_version = ">= 1.12.0"
-  # ToDo: Add R2 storage backend block
+
+  backend "s3" {
+    bucket = "terraform-state"
+    key    = "terraform.tfstate"
+
+    region                      = "auto"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
+
+    endpoints = { s3 = "https://4b612799ee232923f8e7025f0f8af8ff.r2.cloudflarestorage.com" }
+  }
 }
